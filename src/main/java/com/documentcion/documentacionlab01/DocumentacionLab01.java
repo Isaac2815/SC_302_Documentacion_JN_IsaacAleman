7e47001f72e02838ac25b47cb8f5ca9471d371a3
Isaac2815 <isaacalemanb@gmail.com>
2024-10-03 15:20:37 -0600

    profe no entiendo:c

---
+++ src/main/java/com/documentcion/documentacionlab01/DocumentacionLab01.java
@@ -0,0 +1,14 @@
+
+
+package com.documentcion.documentacionlab01;
+
+/**
+ *
+ * @author n
+ */
+public class DocumentacionLab01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+    }
+}
